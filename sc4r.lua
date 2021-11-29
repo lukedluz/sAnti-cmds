2f261be4528a73a32174e19535d4b1d2bb298d8d
Lucas <luke.dluz@gmail.com>
2021-11-29 17:08:09 -0300

    Initial commit

---
+++ sc4r.lua
@@ -0,0 +1,13 @@
+﻿commands = {
+["logout"] = true    -- para adicionar um novo comando, copie o isto: ,[""] = true || Cole embaixo do: ,[""] = true || E coloque o comando que você quer bloquear dentro das aspas.
+,["login"] = true
+,["register"] = true
+,["engine"] = true
+}
+ 
+addEventHandler("onPlayerCommand", getRootElement(),
+function(cmd)
+    if ( commands[cmd] ) then
+        cancelEvent()
+    end
+end)
